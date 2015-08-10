3c4e53198d8d4550e5988424156edc8056f8119b
valentinsvt <valentinsvt@hotmail.com>
2015-08-10 14:32:06 -0500

    programas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -44,7 +44,8 @@
     Route::get('nosotros', 'NosotrosController@index');
     Route::get('ecuador', 'EcuadorController@index');
     Route::get('equipo', 'EquipoController@index');
-
+    Route::get('programas', 'ProgramasController@index');
+    Route::get('programa', 'ProgramasController@programa');
     //    Route::get('frases/{idioma}', array('uses' => 'FrasesController@index'));
     //    Route::get('frases/create', array('uses' => 'FrasesController@create'));
 
